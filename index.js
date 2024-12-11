77e39f6f5cac15d7d7a5bf5ed17aa1df6789553d
Loganv308 <loganv308@gmail.com>
2024-12-10 18:02:52 -0600

    Added deploy-commands.js to assign and reload commands to the bot. Added more utility commands, ping, etc.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-
+// Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const folderPath = path.join(__dirname, 'commands');
-const commandFolder = fs.readdirSync(folderPath, { withFileTypes: true });
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath, { withFileTypes: true });
 
-// The below 'for' loop goes through the comand folder 
-for (const dirent of commandFolder) {
+for (const dirent of commandFolders) {
     if (dirent.isDirectory()) {
-        const commandsPath = path.join(folderPath, dirent.name);
+        const commandsPath = path.join(foldersPath, dirent.name);
         const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
         for (const file of commandFiles) {
             const filePath = path.join(commandsPath, file);
@@ -27,7 +27,7 @@ for (const dirent of commandFolder) {
         }
     } else if (dirent.isFile() && dirent.name.endsWith('.js')) {
         // Direct JS file in the 'commands' folder
-        const filePath = path.join(folderPath, dirent.name);
+        const filePath = path.join(foldersPath, dirent.name);
         const command = require(filePath);
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
@@ -37,17 +37,34 @@ for (const dirent of commandFolder) {
     }
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
 	}
-}
 
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
+});
+
+// Log in to Discord with your client's token
 client.login(token);
