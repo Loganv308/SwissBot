a6023138145ea68f0bcc39f453fdef2ddcbc38b9
loganv308 <loganv308@gmail.com>
2026-05-01 17:11:51 -0500

    Removed FFMPEG from index.js file as it's no longer needed.

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import ffmpeg from '@ffmpeg-installer/ffmpeg';
-process.env.FFMPEG_PATH = ffmpeg.path;
-
-console.log("FFmpeg path:", process.env.FFMPEG_PATH);
-
 import { GatewayIntentBits } from 'discord.js';
 import config from './config.json' with { type: 'json' };
 import BotClient from './BotClient.js';
