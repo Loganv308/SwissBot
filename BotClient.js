591a602e06da861acf66b4b5d81d3d93fba07d43
loganv308 <loganv308@gmail.com>
2026-05-01 20:04:30 -0500

    refactor: remove unnecessary YTDL_PATH setup and Spotify credentials from BotClient.js

--- BotClient.js
+++ BotClient.js
@@ -15,14 +15,7 @@ import { YtDlpExtractor } from './YoutubeDownloader/YtDlpExtractor.js';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const { databaseHost, databaseUser, databasePass, databaseName } = config;
 
-process.env.YTDL_PATH = process.platform === 'win32'
-    ? join(__dirname, 'yt-dlp.exe')
-    : execSync('which yt-dlp').toString().trim();
-
 process.env.DP_FORCE_YTDL_DRIVER = 'yt-dlp';
-
-process.env.SPOTIFY_CLIENT_ID     = config.spotifyClientId;
-process.env.SPOTIFY_CLIENT_SECRET = config.spotifyClientSecret;
 
 process.env.FFMPEG_PATH = 'C:\\Users\\burni\\AppData\\Local\\Microsoft\\WinGet\\Packages\\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\\ffmpeg-8.1-full_build\\bin\\ffmpeg.exe';
 
